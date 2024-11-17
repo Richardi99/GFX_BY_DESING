51b7d53a2dc9f3c3c18cd79797fa1751f0e7a126
Richardi99 <software.barc@outlook.com>
2024-11-16 21:37:01 -0500

    card con animacion 20%

--- src/assets/sections/Service.jsx
+++ src/assets/sections/Service.jsx
@@ -1,31 +1,77 @@
-import React from 'react'
+import React from "react";
+import "../styles/Service.css";
+import MarcoDiv from "../component/MarcoDiv";
 
 function Service() {
   return (
-    <section id='Service'>
-      <div className='Service-container center center height-general M-top' >
-        <h2>Services</h2>
-      <div className="Service-card">
-        <div className="card">
-          <h3>BRANDING</h3>
-          <p>Brands are everwhere, even when we can’t see them. We build lasting brands that convey emotions and values that are right for all audiences.</p>
+    <section id="Service">
+      <div className="Service-container center center height-general M-top">
+          <h2>Services</h2>
+        <div className="Cards">
+          <div className="Service-Card">
+            <MarcoDiv>
+              <div className="first-content">
+                <div className="first-content-text">
+                  <h3>BRANDING</h3>
+                  <p>
+                    Brands are everwhere, even when we can’t see them. We build
+                    lasting brands that convey emotions and values that are
+                    right for all audiences.
+                  </p>
+                </div>
+              </div>
+
+              <div className="second-content">
+                <div className="first-content-text2">
+                  <h3>content2</h3>
+                </div>
+              </div>
+            </MarcoDiv>
+          </div>
+          <div className="Service-Card">
+            <MarcoDiv>
+              <div className="first-content">
+                <div className="first-content-text">
+                  <h3>PRINTING</h3>
+                  <p>
+                    Brands are everwhere, even when we can’t see them. We build
+                    lasting brands that convey emotions and values that are
+                    right for all audiences.
+                  </p>
+                </div>
+              </div>
+
+              <div className="second-content">
+                <div className="first-content-text2">
+                  <h3>content2</h3>
+                </div>
+              </div>
+            </MarcoDiv>
+          </div>
+          <div className="Service-Card">
+            <MarcoDiv>
+              <div className="first-content">
+                <div className="first-content-text">
+                  <h3>GRAPHICS DESIGN</h3>
+                  <p>
+                    Brands are everwhere, even when we can’t see them. We build
+                    lasting brands that convey emotions and values that are
+                    right for all audiences.
+                  </p>
+                </div>
+              </div>
+
+              <div className="second-content">
+                <div className="first-content-text2">
+                  <h3>content2</h3>
+                </div>
+              </div>
+            </MarcoDiv>
+          </div>
         </div>
-      </div>
-      <div className="Service-card">
-      <div className="card">
-          <h3>PRINTING</h3>
-          <p>Brands are everwhere, even when we can’t see them. We build lasting brands that convey emotions and values that are right for all audiences.</p>
-        </div>
-      </div>
-      <div className="Service-card">
-      <div className="card">
-          <h3>GRAPHICS DESIGN</h3>
-          <p>Brands are everwhere, even when we can’t see them. We build lasting brands that convey emotions and values that are right for all audiences.</p>
-        </div>
-      </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Service
+export default Service;
